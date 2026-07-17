64c01d5519b4c1ec51f8709aff75a621874d0c52
jalal84momo-web <jalal84momo@gmail.com>
2026-07-17 03:49:02 +0200

    feat: Add Gradle configuration files and version catalog

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21" apply false
+    id("com.google.hilt.android") version "2.48" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
